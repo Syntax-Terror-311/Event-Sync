d2fc85ac69181a0b9f19effd552a85959d0b7d45
mathieu <mathieu.user24@gmail.com>
2026-05-16 18:54:27 +0300

    feat(api): add health check endpoint

--- event-sync/app/page.tsx
+++ event-sync/app/page.tsx
@@ -1,5 +1,6 @@
-export default function Home() {
-  return (
-    <>Event sync App</>
-  );
+export default async function Home() {
+  const res = await fetch("http://localhost:3000/api/ping");
+  const data = await res.json();
+
+  return <div>{data.message}</div>;
 }
